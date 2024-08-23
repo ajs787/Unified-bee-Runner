0eca1b0debd1f862f0ca0320975d6628eaabb0e1
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-08-23 11:41:12 -0400

    revert to old defaults because ilab sucks

--- Unifier_Run.sh
+++ Unifier_Run.sh
@@ -21,4 +21,4 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-python Unified-bee-Runner/master_run.py --height 720 --width 960 --number-of-samples 40000 --frames-per-sample 5 --gpus 2 --max-workers-video-sampling 10 >> BEERUN.log 2>&1
+python Unified-bee-Runner/master_run.py --height 720 --width 960 --number-of-samples 40000 --frames-per-sample 5 --gpus 2 >> BEERUN.log 2>&1
