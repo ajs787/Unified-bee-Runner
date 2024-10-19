896d7e036e7d84f605dd41507623aba30c351e37
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-10-19 14:20:42 -0400

    work

--- Unifier_Run.sh
+++ Unifier_Run.sh
@@ -22,6 +22,4 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-export PATH="/usr/bin/python3:$PATH"
-
 python3 Unified-bee-Runner/master_run.py --height 720 --width 960 --number-of-samples 1000 --frames-per-sample 5 --gpus 2 >>dataprep.log 2>&1
