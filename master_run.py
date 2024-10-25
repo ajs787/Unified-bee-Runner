75c87a79a97716f3669f54a780eae75f5ad14ba2
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-10-25 09:41:53 -0400

    update quotes

--- master_run.py
+++ master_run.py
@@ -202,7 +202,7 @@ if args.start <= 3 and args.end >= 3:
             shell=True,
         )
 
-        arguments = f"--k {args.k} --model {args.model} --gpus {args.gpus} --seed {args.seed} --width {args.width} --height {args.height} --path_to_file {os.path.join("Unified-bee-Runner","working_bee_analysis")} --frames_per_sample {args.frames_per_sample} --crop_x_offset {args.crop_x_offset} --crop_y_offset {args.crop_y_offset} --epochs {args.epochs}"
+        arguments = f"--k {args.k} --model {args.model} --gpus {args.gpus} --seed {args.seed} --width {args.width} --height {args.height} --path_to_file {os.path.join('Unified-bee-Runner','working_bee_analysis')} --frames_per_sample {args.frames_per_sample} --crop_x_offset {args.crop_x_offset} --crop_y_offset {args.crop_y_offset} --epochs {args.epochs}"
         if args.only_split:
             arguments += " --only_split"
         if args.training_only:
