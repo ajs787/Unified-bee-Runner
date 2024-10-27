fb4bad76267bcb437bd597b0db8b56dc39b85cf0
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-10-27 14:46:54 -0400

    Update Unifier_Run.sh

--- Unifier_Run.sh
+++ Unifier_Run.sh
@@ -28,4 +28,4 @@ cd Unified-bee-Runner
 git submodule update --init --recursive
 cd ..
 
-python3 Unified-bee-Runner/master_run.py --height 720 --width 960 --number-of-samples 1000 --frames-per-sample 5 --gpus 2 >>dataprep.log 2>&1
+python3 Unified-bee-Runner/master_run.py --height 720 --width 960 --number-of-samples 100 --frames-per-sample 5 --gpus 2 >>dataprep.log 2>&1
