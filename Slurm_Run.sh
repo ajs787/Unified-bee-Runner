4242ec441d9ad9da48ac0900c489123046c0f6a1
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-11-18 08:36:13 -0500

    update the slurm runner

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -3,8 +3,8 @@
 echo "submitting slurm job. Remember to edit the Unifier_Run.sh if you want to make changes"
 
 sbatch \
-    -c 8 \
-    -n 4 \
+    -c 10 \
+    -n 8 \
     -G 1 \
     -o dataprep.log \
     Unified-bee-Runner/Unifier_Run.sh
