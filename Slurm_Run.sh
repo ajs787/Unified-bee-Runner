d95447979261523178482c4eeabccdc50049f5af
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-11-21 18:08:09 -0500

    update

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -5,7 +5,7 @@ echo "submitting slurm job. Remember to edit the Unifier_Run.sh if you want to m
 sbatch \
     -n 8 \
     -c 12\
-    -G 1\
+    -G 3\
     --mem=200G \
     --time=28800 \
     -o dataprep.log \
