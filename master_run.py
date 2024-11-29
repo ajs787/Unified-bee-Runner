2dd9f7397d5c51e9e58b99cf12805d9435b540a5
Restyled.io <commits@restyled.io>
2024-11-29 20:02:51 +0000

    Restyled by black

--- master_run.py
+++ master_run.py
@@ -107,8 +107,7 @@ if args.start > args.end:
 
 #  if the videos a .h264, convert to .mp4, else, just make a counts.csv
 if args.start <= 0 and args.end >= 0:
-    logging.info(
-        "(0) Starting the video conversions, always defaulting to .mp4")
+    logging.info("(0) Starting the video conversions, always defaulting to .mp4")
     try:
 
         logging.debug(
@@ -185,8 +184,7 @@ if args.start <= 1 and args.end >= 1:
             )
 
         else:
-            logging.info(
-                "No background subtraction type given, skipping this step")
+            logging.info("No background subtraction type given, skipping this step")
     except Exception as e:
         logging.error(f"Error: {e}")
         raise ValueError("Something went wrong in step 1")
@@ -230,8 +228,7 @@ if args.start <= 2 and args.end >= 2:
                 or file.strip() == "logPos.txt"
                 or file.strip() == "logNeg.txt"
             ]
-            logging.info(
-                f"(2) Creating the dataset with the files: {log_list}")
+            logging.info(f"(2) Creating the dataset with the files: {log_list}")
 
             if args.files is None:
                 string_log_list = ",".join(log_list).strip().replace(" ", "")
@@ -308,8 +305,7 @@ if args.start <= 4 and args.end >= 4:
             f"python3 {os.path.join(DIR_NAME, 'Dataset_Creator/dataset_checker.py')}",
             shell=True,
         )
-        logging.info(
-            "(4) ---- Installing the requirements for the VideoSamplerRewrite")
+        logging.info("(4) ---- Installing the requirements for the VideoSamplerRewrite")
         subprocess.run(
             f"pip install -r {os.path.join(DIR_NAME, 'VideoSamplerRewrite/requirements.txt')} >> /dev/null",
             shell=True,
