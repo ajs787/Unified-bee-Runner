4866cb789a1cffd9bc7ab87833589deb174eb775
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-12-07 22:57:47 -0500

    update prefrences

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -3,8 +3,8 @@
 echo "submitting slurm job. Remember to edit the Unifier_Run.sh if you want to make changes"
 
 sbatch \
-  -n 4 \
-  -c 6 -G 1 --mem=300G \
+  -n 8 \
+  -c 10 -G 1 --mem=600G \
   --time=28800 \
   -o dataprep.log \
   Unified-bee-Runner/Unifier_Run.sh
