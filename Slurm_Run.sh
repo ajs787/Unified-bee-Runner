01b6ce96e6c747e61a3ae4bf613f86f456814f49
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-03-16 18:46:53 -0400

    refine loging for Slurm_Run.sh

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "submitting slurm job. Remember to edit the Unifier_Run.sh if you want to make changes"
+echo "Submitting slurm job. Remember to edit the Unifier_Run.sh if you want to make changes."
 
 sbatch \
   -n 8 \
@@ -9,4 +9,4 @@ sbatch \
   -o dataprep.log \
   Unified-bee-Runner/Unifier_Run.sh
 
-echo "submitted"
+echo "Slurm Submitted"
