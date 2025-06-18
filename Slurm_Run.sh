3fc2f895d0040e67fa7bba650f700c321b9ffebd
Dhruvedd <dhruv.daiya5@gmail.com>
2025-06-18 15:56:44 -0400

    Changed to png based image compression, tar files are generated as expected, bin files are a work in progress

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -3,8 +3,8 @@
 echo "Submitting slurm job. Remember to edit the Unifier_Run.sh if you want to make changes."
 
 sbatch \
-  -n 8 \
-  -c 10 -G 1 --mem=600G \
+  -n 80 \
+  -c 10 -G 4 --mem=600G \
   --time=28800 \
   -o dataprep.log \
   Unified-bee-Runner/Unifier_Run.sh
